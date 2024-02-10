82b6327437e9d30684af6cca26f61c52137a9e22
tamesisblade <53984711+tamesisblade@users.noreply.github.com>
2024-02-10 08:36:36 -0500

    collect

--- app/Http/Controllers/BancoController.php
+++ app/Http/Controllers/BancoController.php
@@ -12,16 +12,58 @@ class BancoController extends Controller
      */
     public function index()
     {
-        $query = Banco::all();
-        $setQuery = collect($query);
-        $resultado = $setQuery->filter(function($item,$key){
-            return $item->siglas == null;
-        })->values();
 
-        //solo obtener los nombres en un array
-        $getOnlyNombres = $resultado->pluck('nombre');
-        return $getOnlyNombres;
+        $collection1 = collect([
+            (object) ['id' => 1, 'name' => 'Object A'],
+            (object) ['id' => 2, 'name' => 'Object B'],
+            (object) ['id' => 3, 'name' => 'Object C'],
+        ]);
+        
+        $collection2 = collect([
+            (object) ['id' => 1, 'name' => 'Object e'],
+            (object) ['id' => 2, 'name' => 'Object B'],
+            (object) ['id' => 4, 'name' => 'Object D'],
+        ]);
+        
+        // Convertir cada objeto a su ID para la comparación
+        $ids1 = $collection1->pluck('id');
+        $ids2 = $collection2->pluck('id');
+        
+        $difference = $ids1->diff($ids2);
+        // Filtrar la colección original para obtener solo los objetos cuyos IDs están en la diferencia
+        $differentObjects = $collection1->filter(function ($item) use ($difference) {
+            return $difference->contains($item->id);
+        });
+        
+        // Mostrar los objetos diferentes
+        foreach ($differentObjects as $obj) {
+            echo "ID: {$obj->id}, Name: {$obj->name}\n";
+        }
+             ///COMBINAR
+            //  $keys = collect(['nombre', 'edad']);
+            //  $values = collect(['Alice', 25]);
+     
+            //  $combined = $keys->combine($values);
+            //  return $combined;
+        // $query = Banco::all();
+        // $collection = collect($query);
+
+        // Dividir la colección en trozos de tamaño 4
+        // $chunks = $collection->chunk(4)->flatten();;
+
+        // return $chunks->all();
+        // $query = Banco::all();
+        // $setQuery = collect($query);
+        // $resultado = $setQuery->filter(function($item,$key){
+        //     return $item->siglas == null;
+        // })->values();
+
+        // //solo obtener los nombres en un array
+        // $getOnlyNombres = $resultado->pluck('nombre');
+        // return $getOnlyNombres;
         // return $query;
+
+        //map
         // $coleccion = collect([
         //     "edad" => "15",
         //     "nombre" => "juan",
@@ -31,7 +73,9 @@ class BancoController extends Controller
         //     if($key == "nombre"){ return $item . " - " . "hola mundo"; }
         //     return $item;
         // });
-         return $resultado;
+        // return $resultado;
+
+ 
     }
 
     /**
